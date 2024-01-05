91392db1d9627bb654cffba6a868daad9eebf14b
Jeremy Sfez <jeremy@smooth-code.com>
2024-01-05 17:22:24 +0100

    test: add Playwright and Argos

---
+++ tests/screenshot-pages.spec.ts
@@ -0,0 +1,17 @@
+import { argosScreenshot } from '@argos-ci/playwright';
+import { test } from '@playwright/test';
+
+const pages = [
+  { name: 'homepage', path: '/' },
+  { name: 'sneakers-list', path: '/sneakers' },
+  { name: 'sneakers-detail', path: '/sneakers/404758' },
+  { name: 'cart', path: '/cart' },
+];
+
+http: test('screenshot pages', async ({ page }) => {
+  for (const { name, path } of pages) {
+    await page.goto(path);
+    await page.waitForTimeout(3000);
+    await argosScreenshot(page, name);
+  }
+});
