4f0a58485452c0c9edea273788ed958eff31545b
Jeremy Sfez <jeremy@smooth-code.com>
2024-01-25 17:22:33 +0100

    feat: update layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,3 @@
 import { twx } from '@/lib/utils';
 
-export const Main = twx.div`max-w-screen-xl mx-auto pt-3 md:pt-16 pb-16`;
+export const Main = twx.div`max-w-screen-xl mx-auto pt-3 md:py-16 pb-16`;
